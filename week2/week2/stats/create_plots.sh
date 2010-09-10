aac1c0fb58f03fe911ab6616163fbf17a5d32325
j_toft@hotmail.com <jtpedersen@users.noreply.github.com>
2010-09-10 19:10:40 +0000

    minor fixme

--- week2/week2/stats/create_plots.sh
+++ week2/week2/stats/create_plots.sh
@@ -2,5 +2,5 @@
 
 
 gnuplot plot.gp
-montage 460GTX_*.png -geometry 800x600\>+2+4 samlet_460.png
-montage 8800_*.png -geometry 800x600\>+2+4 samlet_8800.png
+montage 460GTX*.png -geometry 800x600\>+2+4 samlet_460.png
+montage 8800*.png -geometry 800x600\>+2+4 samlet_8800.png
