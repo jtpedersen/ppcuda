0173402915d2ec58dbaf0d2acc16f55ba73c8bab
j_toft@hotmail.com <j_toft@hotmail.com@2472bca1-7342-608c-6593-5b2de02e154b>
2010-09-23 14:24:51 +0000

    noget rod aargh

--- trunk/dwtHaar1D/dwtImg.h
+++ trunk/dwtHaar1D/dwtImg.h
@@ -7,4 +7,20 @@ void test_img(const char* file);
 __global__
 void simple_copy_kernel(int *in, int *out);
 
+
+__global__
+void to_grayscale_floats(int *in, float *out, int size);
+__global__
+void from_grayscale_floats(float *in, int* out, int size);
+
+__global__ void 
+dwtHaar2D( float* id, float* od, float* approx_final, 
+          const unsigned int dlevels,
+          const unsigned int slength_step_half,
+	   const int bdim );
+
+
+
+
+
 #endif /* !DWTIMG_H_ */
